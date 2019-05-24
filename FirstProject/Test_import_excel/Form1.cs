bbf2d261dc4d75123c361a724b52d6995b208992
EdgarMartirosyan <47984694+EdgarMartirosyan@users.noreply.github.com>
2019-05-24 11:28:12 +0400

    2405_1128

--- FirstProject/Test_import_excel/Form1.cs
+++ FirstProject/Test_import_excel/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using ExcelDataReader;
+
 
 namespace Test_import_excel
 {
@@ -19,7 +22,32 @@ namespace Test_import_excel
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            OpenFileDialog ope = new OpenFileDialog();
+            ope.Filter = "Excel Files| *.xls;*.xlsx;*.xlsm";
+            if (ope.ShowDialog() == DialogResult.Cancel)
+                return;
+            FileStream stream = new FileStream(ope.FileName, FileMode.Open);
+            IExcelDataReader excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+            DataSet result = excelReader.AsDataSet();
+            DataClasses1DataContext conn = new DataClasses1DataContext();
+            foreach (DataTable table in result.Tables)
+            {
+                foreach (DataRow dr in table.Rows)
+                {
+                    Test addtabl = new Test()
+                    {
+                        test_id=Convert.ToString(dr[0]),
+                        test_name=Convert.ToString(dr[1]),
+                        test_surname=Convert.ToString(dr[2]),
+                        test_age=Convert.ToInt32(dr[3]),                        
+                    };
+                    conn.Tests.InsertOnSubmit(addtabl);
+                }
+            }
+            conn.SubmitChanges();
+            excelReader.Close();
+            stream.Close();
+            MessageBox.Show("lyox lyava");
         }
     }
 }
